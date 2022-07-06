0e4040ecfe44e022ec8cef018a95f6799866195a
Amirshokh Mukhitdinov <amirshoxmuxitdinov@gmail.com>
2022-07-06 23:59:17 +0500

    created models, schemas

--- schemas/schema.article.go
+++
@@ -1,8 +0,0 @@
-package schemas
-
-type SchemaArticle struct {
-	ID     string `json:"id" validate:"uuid"`
-	Title  string `json:"title" validate:"required,lowercase"`
-	Body   string `json:"body" validate:"required,lowercase"`
-	Author string `json:"author" validate:"required,lowercase"`
-}
